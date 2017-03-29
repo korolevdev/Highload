db9c94b649739308c960536063c1de8c1165c5de
korolevdev <korolevdev@gmail.com>
2017-03-29 10:31:14 +0300

    added gen_server template

--- src/server.erl
+++ src/server.erl
@@ -1,3 +1,16 @@
 -module(server).
 
 -behaviour(gen_server).
+
+-export([start_link/0, code_change/3, handle_call/3, handle_cast/2, handle_info/2, init/1, terminate/2]).
+
+
+start_link() -> gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+init([]) -> {ok, State}.
+
+handle_cast(_Msg, State) -> {noreply, State}.
+handle_call(_Request, _From, State) -> {noreply, State}.
+handle_info(_Info, State) ->  {noreply, State}.
+terminate(_Reason, _State) -> ok.
+code_change(_OldVsn, State, _Extra) -> {ok, State}.
